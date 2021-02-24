e092930a13471ad5fa9b6e02257f36d6fc2e1153
Aminul Islam <aminul.iiuc@gmail.com>
2021-02-24 15:11:29 +0600

    Update deploy.sh
    update

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-echo "Kill all the running PM2 actions"
+#echo "Kill all the running PM2 actions"
 #sudo pm2 kill
 
 echo "Jump to app folder"
@@ -12,7 +12,7 @@ sudo npm run build
 #sudo docker-compose up --build 
 #&& sudo docker-compose up
 
-echo "Run new PM2 action"
+#echo "Run new PM2 action"
 sudo npm run start
 #sudo cp /home/ubuntu/ecosystem.json ecosystem.json
 #sudo pm2 start
