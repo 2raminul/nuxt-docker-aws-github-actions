8b96d26a85767a202110a8175c8bf6536189f3fd
2raminul <aminul.iiuc@gmail.com>
2021-02-24 13:59:50 +0600

    new deploy file added

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+echo "Kill all the running PM2 actions"
+sudo pm2 kill
+
+echo "Jump to app folder"
+cd /home/ubuntu/nuxt-docker-aws-github-actions
+#sudo docker-compose down
+
+echo "Update app from Git"
+git pull origin 
+sudo npm install
+sudo npm run build
+#sudo docker-compose up --build 
+#&& sudo docker-compose up
+
+
+echo "Run new PM2 action"
+sudo cp /home/ubuntu/ecosystem.json ecosystem.json
+sudo pm2 start
